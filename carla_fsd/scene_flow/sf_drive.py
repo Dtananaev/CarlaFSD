257cf70c5ab96c54d5a55977b49476f57c4fbc31
Denis Tananaev <d.d.tananaev@gmail.com>
2024-03-24 15:36:02 +0100

    added scene flow

---
+++ carla_fsd/scene_flow/sf_drive.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2024 Tananaev Denis
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+import glob
+import os
+import sys
+from carla_fsd.scene_flow.tools.optical_flow_visu import point_vec
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+
+import random
+
+try:
+    import pygame
+except ImportError:
+    raise RuntimeError('cannot import pygame, make sure pygame package is installed')
+
+try:
+    import numpy as np
+except ImportError:
+    raise RuntimeError('cannot import numpy, make sure numpy package is installed')
+
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
+
+IMAGE_WIDTH =800
+IMAGE_HEIGHT =600
+
+
+class CarlaSyncMode(object):
+    """
+    Context manager to synchronize output from different sensors. Synchronous
+    mode is enabled as long as we are inside this context
+
+        with CarlaSyncMode(world, sensors) as sync_mode:
+            while True:
+                data = sync_mode.tick(timeout=1.0)
+
+    """
+
+    def __init__(self, world, *sensors, **kwargs):
+        self.world = world
+        self.sensors = sensors
+        self.frame = None
+        self.delta_seconds = 1.0 / kwargs.get('fps', 20)
+        self._queues = []
+        self._settings = None
+
+    def __enter__(self):
+        self._settings = self.world.get_settings()
+        self.frame = self.world.apply_settings(carla.WorldSettings(
+            no_rendering_mode=False,
+            synchronous_mode=True,
+            fixed_delta_seconds=self.delta_seconds))
+
+        def make_queue(register_event):
+            q = queue.Queue()
+            register_event(q.put)
+            self._queues.append(q)
+
+        make_queue(self.world.on_tick)
+        for sensor in self.sensors:
+            make_queue(sensor.listen)
+        return self
+
+    def tick(self, timeout):
+        self.frame = self.world.tick()
+        data = [self._retrieve_data(q, timeout) for q in self._queues]
+        assert all(x.frame == self.frame for x in data)
+        return data
+
+    def __exit__(self, *args, **kwargs):
+        self.world.apply_settings(self._settings)
+
+    def _retrieve_data(self, sensor_queue, timeout):
+        while True:
+            data = sensor_queue.get(timeout=timeout)
+            if data.frame == self.frame:
+                return data
+
+
+def draw_image(surface, image, blend=False):
+    array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+    array = np.reshape(array, (image.height, image.width, 4))
+    array = array[:, :, :3]
+    array = array[:, :, ::-1]
+    image_surface = pygame.surfarray.make_surface(array.swapaxes(0, 1))
+    if blend:
+        image_surface.set_alpha(100)
+    surface.blit(image_surface, (0, 0))
+
+
+def get_font():
+    fonts = [x for x in pygame.font.get_fonts()]
+    default_font = 'ubuntumono'
+    font = default_font if default_font in fonts else fonts[0]
+    font = pygame.font.match_font(font)
+    return pygame.font.Font(font, 14)
+
+
+def should_quit():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                return True
+    return False
+
+
+def get_image(image_rgb):
+    """Gets rgb image."""
+    array = np.frombuffer(image_rgb.raw_data, dtype=np.dtype("uint8"))
+    array = np.reshape(array, (image_rgb.height, image_rgb.width, 4))
+    array = array[:, :, :3]
+    array = array[:, :, ::-1]
+    return array
+
+def get_optical_flow(image_flow, forward_flow =True):
+    """Gets optical flow."""
+    array = np.frombuffer(image_flow.raw_data, dtype=np.float32)
+    array = np.reshape(array, (image_flow.height, image_flow.width, 2))
+    array = array.copy()
+    # Forward flow
+    if forward_flow:
+        array[..., 0] *= 0.5 * image_flow.width
+        array[..., 1] *= -0.5 * image_flow.height
+    else:
+        array[..., 0] *= -0.5 * image_flow.width
+        array[..., 1] *= 0.5 * image_flow.height    
+    
+    return array
+    
+def get_depth(image_depth):
+    """Gets depth."""
+    def _decode_depth(depth):
+        depth = np.asarray(depth, dtype = np.float32)
+        d = depth[:,:,0] + depth[:,:,1]*256 + depth[:,:,2]*256*256
+        # Normalize between 0 and 1
+        d = d /  ( 256*256*256 - 1 )
+        # The far plane set 1000 to restore meters
+        return d * 1000 
+
+    array = np.frombuffer(image_depth.raw_data, dtype=np.uint8)
+    array = np.reshape(array, (image_depth.height, image_depth.width, 4))
+    array = array[:, :, :3]
+    array = array[:, :, ::-1]
+    return _decode_depth(array)
+
+def main():
+    output_dir = "./outputs"
+
+
+    images_dir = os.path.join(output_dir, "rgb")
+    depth_dir = os.path.join(output_dir, "depth")
+    flow_dir = os.path.join(output_dir, "flow")
+    os.makedirs(images_dir, exist_ok=True)
+    os.makedirs(depth_dir, exist_ok=True)
+    os.makedirs(flow_dir, exist_ok=True)
+
+    actor_list = []
+    pygame.init()
+
+
+    display = pygame.display.set_mode(
+        (IMAGE_WIDTH, IMAGE_HEIGHT),
+        pygame.HWSURFACE | pygame.DOUBLEBUF)
+    font = get_font()
+    clock = pygame.time.Clock()
+
+    client = carla.Client('localhost', 2000)
+    client.set_timeout(2.0)
+
+    world = client.get_world()
+
+    try:
+        m = world.get_map()
+        start_pose = random.choice(m.get_spawn_points())
+        waypoint = m.get_waypoint(start_pose.location)
+
+        blueprint_library = world.get_blueprint_library()
+
+        vehicle = world.spawn_actor(
+            random.choice(blueprint_library.filter('vehicle.tesla.model3')),
+            start_pose)
+        actor_list.append(vehicle)
+        vehicle.set_simulate_physics(False)
+
+        x_pose = 2.40
+        z_pose = 1.5
+
+        camera_blueprint = blueprint_library.find('sensor.camera.rgb')
+        camera_blueprint.set_attribute('image_size_x', str(IMAGE_WIDTH))
+        camera_blueprint.set_attribute('image_size_y', str(IMAGE_HEIGHT))
+        camera_blueprint.set_attribute('fov', "90")
+        camera_rgb = world.spawn_actor(camera_blueprint
+            ,
+            carla.Transform(carla.Location(x=x_pose, z=z_pose), carla.Rotation(pitch=0)),
+            attach_to=vehicle)
+        actor_list.append(camera_rgb)
+
+        camera_blueprint =   blueprint_library.find('sensor.camera.optical_flow')
+        camera_blueprint.set_attribute('image_size_x', str(IMAGE_WIDTH))
+        camera_blueprint.set_attribute('image_size_y', str(IMAGE_HEIGHT))
+        camera_blueprint.set_attribute('fov', "90")
+        camera_optical_flow = world.spawn_actor(
+          camera_blueprint,
+            carla.Transform(carla.Location(x=x_pose, z=z_pose), carla.Rotation(pitch=0)),
+            attach_to=vehicle)
+        actor_list.append(camera_optical_flow)
+
+
+        camera_blueprint =  blueprint_library.find('sensor.camera.depth')
+        camera_blueprint.set_attribute('image_size_x', str(IMAGE_WIDTH))
+        camera_blueprint.set_attribute('image_size_y', str(IMAGE_HEIGHT))
+        camera_blueprint.set_attribute('fov', "90")
+        camera_depth = world.spawn_actor(
+            camera_blueprint,
+            carla.Transform(carla.Location(x=x_pose, z=z_pose), carla.Rotation(pitch=0)),
+            attach_to=vehicle)
+        actor_list.append(camera_depth)
+
+
+        # Create a synchronous mode context.
+        with CarlaSyncMode(world, camera_rgb, camera_optical_flow, camera_depth, fps=30) as sync_mode:
+            while True:
+                if should_quit():
+                    return
+                clock.tick()
+
+                # Advance the simulation and wait for the data.
+                snapshot, image_rgb, image_flow, image_depth = sync_mode.tick(timeout=2.0)
+                image = get_image(image_rgb)
+                optical_flow = get_optical_flow(image_flow, forward_flow=True)
+
+                optical_flow_visu = np.concatenate((optical_flow, np.ones((IMAGE_HEIGHT, IMAGE_WIDTH, 1))), axis=-1)
+                flow_rgb = point_vec(image, optical_flow_visu,skip=30)
+                depth = get_depth(image_depth)
+                #print(f"depth  min {np.min(depth)} max {np.max(depth)}")
+
+                #print(f"optical_flow  min {np.min(optical_flow)} max {np.max(optical_flow)}")
+
+                #print(f"depth {depth.shape}, optical_flow {optical_flow.shape}")
+
+                # Choose the next waypoint and update the car location.
+                waypoint = random.choice(waypoint.next(1.5))
+                vehicle.set_transform(waypoint.transform)
+
+                fps = round(1.0 / snapshot.timestamp.delta_seconds)
+
+                # Draw the display.
+                display.blit(pygame.surfarray.make_surface(flow_rgb.swapaxes(0, 1)), (0, 0))
+                pygame.display.flip()
+                pygame.event.pump()           
+                display.blit(
+                    font.render('% 5d FPS (real)' % clock.get_fps(), True, (255, 255, 255)),
+                    (8, 10))
+                display.blit(
+                    font.render('% 5d FPS (simulated)' % fps, True, (255, 255, 255)),
+                    (8, 28))
+                pygame.display.flip()
+
+    finally:
+
+        print('destroying actors.')
+        for actor in actor_list:
+            actor.destroy()
+
+        pygame.quit()
+        print('done.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ carla_fsd/scene_flow/tools/optical_flow_visu.py
@@ -0,0 +1,184 @@
+# Copyright (c) 2024 Tananaev Denis
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+import numpy as np
+import cv2
+
+UNKNOWN_FLOW_THRESH = 1e7
+
+def point_vec(img, flow, skip=2):
+    maxsize = 100.
+    extendfac = 1.
+
+    resize_factor = max(1, int(max(maxsize/img.shape[0], maxsize/img.shape[1])))
+    dispimg = cv2.resize(img.copy(), None, fx=resize_factor, fy=resize_factor)
+    colorflow = flow_to_image(flow).astype(int)
+
+    rows, cols, _ = img.shape
+    y_range, x_range =np.meshgrid(range(rows), range(cols), indexing='ij')
+    y_range = np.reshape(y_range, -1)
+    x_range = np.reshape(x_range, -1)
+    flow = np.reshape(flow, (-1, 3))
+    mask = (y_range % skip == 0) & (x_range  % skip == 0)
+
+    x_coords, y_coords, flow_coords = x_range[mask],y_range[mask], flow[mask, :]
+
+
+
+    mesh_coords = np.stack([x_coords, y_coords], axis=-1)
+
+    mesh_coords_warped = (mesh_coords + extendfac * flow_coords[..., :2]).astype(int)
+    mesh_coords_warped *= resize_factor
+    lengths = np.linalg.norm(flow_coords[..., :2] * extendfac, axis=-1)
+    for k in range(mesh_coords_warped.shape[0]):
+        i, j = mesh_coords[k]
+        xend, yend = mesh_coords_warped[k]
+        leng = lengths[k]
+
+        if leng < 1:
+            continue
+
+        dispimg = cv2.arrowedLine(dispimg, (i * resize_factor, j * resize_factor),
+                                  (xend, yend),
+                                  (int(colorflow[j, i, 0]), int(colorflow[j, i, 1]), int(colorflow[j, i, 2])), 4,
+                                  tipLength=2/leng, line_type=cv2.LINE_AA)
+
+    return dispimg
+
+
+
+
+
+def flow_to_image(flow):
+    """
+    Convert flow into middlebury color code image
+    :param flow: optical flow map
+    :return: optical flow image in middlebury color
+    """
+    u = flow[:, :, 0]
+    v = flow[:, :, 1]
+
+    maxu = -999.
+    maxv = -999.
+    minu = 999.
+    minv = 999.
+
+    idxUnknow = (abs(u) > UNKNOWN_FLOW_THRESH) | (abs(v) > UNKNOWN_FLOW_THRESH)
+    u[idxUnknow] = 0
+    v[idxUnknow] = 0
+
+    maxu = max(maxu, np.max(u))
+    minu = min(minu, np.min(u))
+
+    maxv = max(maxv, np.max(v))
+    minv = min(minv, np.min(v))
+
+    rad = np.sqrt(u ** 2 + v ** 2)
+    maxrad = max(-1, np.max(rad))
+
+    u = u/(maxrad + np.finfo(float).eps)
+    v = v/(maxrad + np.finfo(float).eps)
+
+    img = compute_color(u, v)
+
+    idx = np.repeat(idxUnknow[:, :, np.newaxis], 3, axis=2)
+    img[idx] = 0
+
+    return np.uint8(img)
+
+
+
+def compute_color(u, v):
+    """
+    compute optical flow color map
+    :param u: optical flow horizontal map
+    :param v: optical flow vertical map
+    :return: optical flow in color code
+    """
+    [h, w] = u.shape
+    img = np.zeros([h, w, 3])
+    nanIdx = np.isnan(u) | np.isnan(v)
+    u[nanIdx] = 0
+    v[nanIdx] = 0
+
+    colorwheel = make_color_wheel()
+    ncols = np.size(colorwheel, 0)
+
+    rad = np.sqrt(u**2+v**2)
+
+    a = np.arctan2(-v, -u) / np.pi
+
+    fk = (a+1) / 2 * (ncols - 1) + 1
+
+    k0 = np.floor(fk).astype(int)
+
+    k1 = k0 + 1
+    k1[k1 == ncols+1] = 1
+    f = fk - k0
+
+    for i in range(0, np.size(colorwheel,1)):
+        tmp = colorwheel[:, i]
+        col0 = tmp[k0-1] / 255
+        col1 = tmp[k1-1] / 255
+        col = (1-f) * col0 + f * col1
+
+        idx = rad <= 1
+        col[idx] = 1-rad[idx]*(1-col[idx])
+        notidx = np.logical_not(idx)
+
+        col[notidx] *= 0.75
+        img[:, :, i] = np.uint8(np.floor(255 * col*(1-nanIdx)))
+
+    return img
+
+
+def make_color_wheel():
+    """
+    Generate color wheel according Middlebury color code
+    :return: Color wheel
+    """
+    RY = 15
+    YG = 6
+    GC = 4
+    CB = 11
+    BM = 13
+    MR = 6
+
+    ncols = RY + YG + GC + CB + BM + MR
+
+    colorwheel = np.zeros([ncols, 3])
+
+    col = 0
+
+    # RY
+    colorwheel[0:RY, 0] = 255
+    colorwheel[0:RY, 1] = np.transpose(np.floor(255*np.arange(0, RY) / RY))
+    col += RY
+
+    # YG
+    colorwheel[col:col+YG, 0] = 255 - np.transpose(np.floor(255*np.arange(0, YG) / YG))
+    colorwheel[col:col+YG, 1] = 255
+    col += YG
+
+    # GC
+    colorwheel[col:col+GC, 1] = 255
+    colorwheel[col:col+GC, 2] = np.transpose(np.floor(255*np.arange(0, GC) / GC))
+    col += GC
+
+    # CB
+    colorwheel[col:col+CB, 1] = 255 - np.transpose(np.floor(255*np.arange(0, CB) / CB))
+    colorwheel[col:col+CB, 2] = 255
+    col += CB
+
+    # BM
+    colorwheel[col:col+BM, 2] = 255
+    colorwheel[col:col+BM, 0] = np.transpose(np.floor(255*np.arange(0, BM) / BM))
+    col += + BM
+
+    # MR
+    colorwheel[col:col+MR, 2] = 255 - np.transpose(np.floor(255 * np.arange(0, MR) / MR))
+    colorwheel[col:col+MR, 0] = 255
+
+    return colorwheel
